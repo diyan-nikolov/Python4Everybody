3c2d4debf46bec565b037a10e3ac15b5b1fddbf3
Diyan Nikolov <dev.dnikolov@gmail.com>
2021-03-16 13:57:42 -0400

    Update #3

--- Python For Everybody/ex_03_01.py
+++ Python For Everybody/ex_03_01.py
@@ -1,3 +1,5 @@
+
+
 hrs = input("Enter Hours:")
 hrs = float(hrs)
 pph = input("Enter Rate:")
@@ -7,3 +9,4 @@ if hrs<= 40 :
     print(hrs * pph)
 else : 
     print(((hrs-40)*1.5*pph) + (40*pph))
+    print("Done")
